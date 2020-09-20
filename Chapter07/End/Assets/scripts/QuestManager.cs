ae5fcda1bf4e6599b7f73693f9c0f28b233e0820
Robert Wells <robert.wells@thatgamesguy.co.uk>
2020-09-20 12:57:07 +0100

    Moved Quest class to separate file

--- Chapter07/End/Assets/scripts/QuestManager.cs
+++ Chapter07/End/Assets/scripts/QuestManager.cs
@@ -1,14 +1,5 @@
 ﻿using UnityEngine;
 
-[System.Serializable]
-public class Quest
-{
-	//Quest completed status
-	public enum QUESTSTATUS {UNASSIGNED=0,ASSIGNED=1,COMPLETE=2};
-	public QUESTSTATUS Status = QUESTSTATUS.UNASSIGNED;
-	public string QuestName = string.Empty;
-}
-
 public class QuestManager : MonoBehaviour
 {
 	public Quest[] Quests;
