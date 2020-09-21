820f087f06ac41646d1ff3f6d79b100827d5b584
Robert Wells <robert.wells@thatgamesguy.co.uk>
2020-09-21 13:21:01 +0100

    updated Quest class

--- Chapter07/End/Assets/Scripts/Quest.cs
+++ Chapter07/End/Assets/Scripts/Quest.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Quest
 {
 	//Quest completed status
-	public enum QUESTSTATUS { UNASSIGNED = 0, ASSIGNED = 1, COMPLETE = 2 };
+	public enum QUESTSTATUS { UNASSIGNED, ASSIGNED, COMPLETE };
 	public QUESTSTATUS Status = QUESTSTATUS.UNASSIGNED;
 	public string QuestName = string.Empty;
 }
